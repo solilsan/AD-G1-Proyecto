bac8920fb316f4774c1eeedd04c4f484e6160081
m <mikel.gonzalez@ikasle.egibide.org>
2020-10-06 21:55:55 +0200

    Añadidas nuevas funciones al controlador clientes

--- src/Pruebas/SQLiteTest.java
+++ src/Pruebas/SQLiteTest.java
@@ -3,11 +3,21 @@ package Pruebas;
 import SQLite.ControladorCliente;
 import Clases.Cliente;
 
+import java.sql.Connection;
 import java.util.ArrayList;
 
 public class SQLiteTest {
 
     public static void main(String[] args) {
+        //insertCliente();
+        //selectWhereCliente();
+        //darClienteDeBaja();
+       // selectAllClientes();
+       // darClienteDeAlta();
+
+    }
+
+    public static void selectAllClientes(){
         ArrayList<Cliente> clientes = ControladorCliente.selectAll();
 
         // Comprobamos que no el array list no esta vacio
@@ -23,6 +33,41 @@ public class SQLiteTest {
         }else{
             System.out.println("[SQLite - Select All] La tabla esta vacia, o ha ocurrido un error.");
         }
+    }
+
+    public static void insertCliente(){
+        ControladorCliente.insertCliente("123asd","pepe","dfd","2020-03-03","una profesion","activo");
 
     }
+
+    public static void selectWhereCliente(){
+        ArrayList<Cliente> clientes = ControladorCliente.selectWhere("APELLIDO", "dfd");
+
+        // Comprobamos que no el array list no esta vacio
+        if (!clientes.isEmpty()){
+            for (int i = 0; i < clientes.size(); i++) {
+                System.out.println("\nDNI: " + clientes.get(i).getDni() +
+                        "\nNombre: " + clientes.get(i).getNombre() +
+                        "\nApellido: " + clientes.get(i).getApellidos() +
+                        "\nFecha Nacimiento: " + clientes.get(i).getFechaNacimiento() +
+                        "\nProfesion: " +  clientes.get(i).getProfesion() +
+                        "\nEstado: " +  clientes.get(i).getEstado());
+            }
+        }else{
+            System.out.println("[SQLite - Select All] La tabla esta vacia, o ha ocurrido un error.");
+        }
+    }
+
+    public static void darClienteDeBaja(){
+        ControladorCliente.darBajaCliente(new Cliente("123asd",null,null,null,null,null,null));
+    }
+
+    public static void darClienteDeAlta(){
+        ControladorCliente.darAltaCliente(new Cliente("123asd",null,null,null,null,null,null));
+    }
+
+    public static void actualizarUnoWhere(){
+        ControladorCliente.updateUnCampo("PROFESION","")
+    }
+
 }
--- src/SQLite/ControladorCliente.java
+++ src/SQLite/ControladorCliente.java
@@ -2,29 +2,25 @@ package SQLite;
 
 import Clases.Cliente;
 
-import java.awt.desktop.QuitEvent;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
-public class ControladorCliente extends Conexion{
+public class ControladorCliente extends Conexion {
 
-    public static ArrayList<Cliente> selectAll(){
+    public static ArrayList<Cliente> selectAll() {
         Connection conn = conn();
         ArrayList<Cliente> resultado = new ArrayList<>();
 
         // Comprobamos que la conexion haya tenido exito.
         // Si sale null es que ha habido un error.
-        if (conn != null){
+        if (conn != null) {
             String query = "SELECT * FROM CLIENTES";
 
             try {
                 Statement stmt = conn.createStatement();
                 ResultSet rs = stmt.executeQuery(query);
 
-                while (rs.next()){
+                while (rs.next()) {
                     resultado.add(new Cliente(rs.getString("DNI"), rs.getString("NOMBRE"),
                             rs.getString("APELLIDO"), rs.getDate("FECHA_NAC").toString(),
                             rs.getString("PROFESION"), rs.getString("ESTADO"), null));
@@ -32,7 +28,7 @@ public class ControladorCliente extends Conexion{
 
                 conn.close();
             } catch (SQLException throwables) {
-                System.out.println("[SQLite - ControladorCliente] Error: \n "+
+                System.out.println("[SQLite - ControladorCliente] Error: \n " +
                         throwables.getMessage());
             }
         }
@@ -42,4 +38,164 @@ public class ControladorCliente extends Conexion{
         return resultado;
     }
 
+    public static Boolean insertCliente(String DNI, String NOMBRE, String APELLIDO, String FECHA_NAC, String PROFESION, String ESTADO) {
+        Connection conn = conn();
+        String query = "INSERT INTO CLIENTES (DNI, NOMBRE, APELLIDO, FECHA_NAC, PROFESION, ESTADO) VALUES(?, ?, ?, ?, ?, ?)";
+
+
+        try {
+            PreparedStatement sentencia = conn.prepareStatement(query);
+
+            // Introducimos los datos
+            sentencia.setString(1, DNI);
+            sentencia.setString(2, NOMBRE);
+            sentencia.setString(3, APELLIDO);
+            sentencia.setDate(4, Date.valueOf(FECHA_NAC));
+            sentencia.setString(5, PROFESION);
+            sentencia.setString(6, ESTADO);
+
+            // Ejecutamos la sentencia
+            Integer res = sentencia.executeUpdate();
+
+            if (res > 0) {
+                return true;
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        // Si hemos llegado aqui, es que algo malo ha pasado.
+        return false;
+    }
+
+    public static ArrayList<Cliente> selectWhere(String parametro, String tiene) {
+        Connection conn = conn();
+        ArrayList<Cliente> listaClientes = new ArrayList<>();
+        String query = "SELECT * FROM CLIENTES WHERE "+ parametro +" = ?";
+
+
+        try {
+            PreparedStatement sentencia = conn.prepareStatement(query);
+
+            sentencia.setString(1, tiene);
+
+            ResultSet rs = sentencia.executeQuery();
+
+            while (rs.next()) {
+                listaClientes.add(new Cliente(rs.getString("DNI"), rs.getString("NOMBRE"),
+                        rs.getString("APELLIDO"), rs.getDate("FECHA_NAC").toString(),
+                        rs.getString("PROFESION"), rs.getString("ESTADO"), null));
+            }
+
+            return listaClientes;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return listaClientes;
+    }
+
+    /**
+     * ESTA FUNCION, Y LA DE ALTA, SE PODRIAN FUSIONAR EN UNA
+     * TODO: POSIBLE OPTIMIZACION.
+     */
+    public static boolean darBajaCliente(Cliente objCliente){
+        Connection conn = conn();
+        String query = "UPDATE CLIENTES SET ESTADO = 'baja' WHERE DNI = ?";
+
+        // Comprobamos que el DNI del objeto no este vacio
+        if (objCliente.getDni().equals("") || objCliente.getDni() == null){
+            // No se puede seguir sin un DNI
+            System.out.println("[SQLite - ControladorCliente] Se ha intentado dar de baja un cliente, pero se ha\n" +
+                    "pasado un objeto cliente sin DNI.");
+            return false;
+        }
+
+        try {
+            PreparedStatement sentencia = conn.prepareStatement(query);
+
+            // Introducimos los datos
+            sentencia.setString(1, objCliente.getDni());
+
+            // Ejecutamos la sentencia
+            Integer res = sentencia.executeUpdate();
+
+            if (res > 0) {
+                return true;
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        // Si hemos llegado aqui, es que algo malo ha pasado.
+        return false;
+    }
+
+    public static boolean darAltaCliente(Cliente objCliente){
+        Connection conn = conn();
+        String query = "UPDATE CLIENTES SET ESTADO = 'alta' WHERE DNI = ?";
+
+        // Comprobamos que el DNI del objeto no este vacio
+        if (objCliente.getDni().equals("") || objCliente.getDni() == null){
+            // No se puede seguir sin un DNI
+            System.out.println("[SQLite - ControladorCliente] Se ha intentado dar de alta un cliente, pero se ha\n" +
+                    "pasado un objeto cliente sin DNI.");
+            return false;
+        }
+
+        try {
+            PreparedStatement sentencia = conn.prepareStatement(query);
+
+            // Introducimos los datos
+            sentencia.setString(1, objCliente.getDni());
+
+            // Ejecutamos la sentencia
+            Integer res = sentencia.executeUpdate();
+
+            if (res > 0) {
+                return true;
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        // Si hemos llegado aqui, es que algo malo ha pasado.
+        return false;
+    }
+
+
+    /**
+     * ATENCION !!!!! NO SE LE PUEDE PASAR EL CAMPO DE WHERE CON ALGO QUE SEA DISTINTO DE STRING
+     */
+    public static boolean updateUnCampo(String campo, String valor, String campoWhere, String valorWhere){
+        Connection conn = conn();
+        String query = "UPDATE CLIENTES SET "+ campo +" = 'alta' WHERE "+ campoWhere +" = ?";
+
+        // Comprobamos que el DNI del objeto no este vacio
+        if (campo.equals("") || campo == null || campoWhere.equals("") || valorWhere == null){
+            // No se puede seguir sin un DNI
+            System.out.println("[SQLite - ControladorCliente] Se ha intentado alterar un campo del cliente, pero el parametro es\n" +
+                    "nulo, o esta vacio.");
+            return false;
+        }
+
+        try {
+            PreparedStatement sentencia = conn.prepareStatement(query);
+
+            // Introducimos los datos
+            sentencia.setString(1, valor);
+            sentencia.setString(2, valorWhere);
+
+            // Ejecutamos la sentencia
+            Integer res = sentencia.executeUpdate();
+
+            if (res > 0) {
+                return true;
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        // Si hemos llegado aqui, es que algo malo ha pasado.
+        return false;
+    }
 }
