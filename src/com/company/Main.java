903cbe89452874f456b2e50247e8494d0539242b
solilsan <47690529+solilsan@users.noreply.github.com>
2020-10-09 10:03:24 +0200

    delete out

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,5 @@
 package com.company;
 
-import Clases.Cliente;
-import Clases.Empleado;
-import Clases.Visita;
-import DB4O.ModeloCliente;
-
 public class Main {
 
     public static void main(String[] args) {
